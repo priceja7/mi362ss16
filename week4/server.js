bf152837e0c4ce8c98c972b125e17d55d0162148
priceja7 <Bubba944>
2016-02-09 01:01:57 -0500

    server.js hello world week4 done

--- week4/server.js
+++ week4/server.js
@@ -10,11 +10,7 @@ http.createServer( (request, response) => {
     response.end();
 }).listen(8124);
 
-http.createServer(function(request, response) {
-    response.writeHeader(200, {"Content-Type": "text/html"});
-    response.write(html);
-    response.end();
-}).listen(8000);
+
 
 console.log('Server running at http://127.0.0.1:8124/');
 
