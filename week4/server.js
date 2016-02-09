f2c5fde0109f05f8a8a12a8940fb016df6030973
priceja7 <Bubba944>
2016-02-09 01:01:21 -0500

    server.js hello world week4 done

--- week4/server.js
+++ week4/server.js
@@ -4,9 +4,17 @@
 const http = require('http');
 
 http.createServer( (request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-response.end('Hello World\n');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<h1>Hello World</h1>')
+    response.write('<p>This is a node app!</p>')
+    response.end();
 }).listen(8124);
+
+http.createServer(function(request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(html);
+    response.end();
+}).listen(8000);
 
 console.log('Server running at http://127.0.0.1:8124/');
 
